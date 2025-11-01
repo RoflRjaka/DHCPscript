ffab24637d7f591c4079af1049f2f9f26f246042
RoflRjaka <mrnoob45297@gmail.com>
2025-11-01 10:24:00 +0500

    Add files via upload

---
+++ server.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+[[ $EUID -ne 0 ]] && { echo "Запустите от root"; exit 1; }
+read -rp "Введите Фамилию и Имя (пример: Петров Иван): " f i _ || exit 1
+[[ -z $f || -z $i ]] && { echo "Ошибка: укажите Фамилию и Имя"; exit 1; }
+full_surname="$f"
+f=${f::1} i=${i::1}
+[[ $f$i =~ ^[А-Яа-яЁё]{2}$ ]] || { echo "Ошибка: используйте кириллицу"; exit 1; }
+ALPH=АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ
+X=$(expr index "$ALPH" "${f^^}") Y=$(expr index "$ALPH" "${i^^}")
+((X && Y)) || { echo "Ошибка: буква не в алфавите"; exit 1; }
+LATIN_SURNAME=$(tr 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ' \
+                    'abvgdeejzijklmnoprstufkhczshsh_y_euaABVGDEEJZIJKLMNOPRSTUFKHCZSHSH_Y_EUA' <<< "$full_surname" |
+                tr -cd '[:alnum:]' || exit 1)
+[[ -z $LATIN_SURNAME ]] && { echo "Ошибка: пустая фамилия после транслитерации"; exit 1; }
+echo "${LATIN_SURNAME}_SRV" | sudo tee /etc/hostname > /dev/null
+sudo hostnamectl set-hostname "${LATIN_SURNAME}_SRV"
+
+apt update -qq
+apt install -y -qq nano sudo syslog-ng isc-dhcp-server || { echo "Ошибка установки пакетов"; exit 1; }
+cat > /etc/network/interfaces <<EOF
+source /etc/network/interfaces.d/*
+auto lo
+iface lo inet loopback
+allow-hotplug enp0s3
+iface enp0s3 inet static
+    address 10.$X.$Y.10
+    netmask 255.255.255.0
+EOF
+systemctl restart networking
+
+cat > /etc/default/isc-dhcp-server <<EOF
+INTERFACESv4="enp0s3"
+INTERFACESv6=""
+EOF
+
+cat > /etc/dhcp/dhcpd.conf <<EOF
+option domain-name "shusha.ru";
+option domain-name-servers 10.$X.$Y.10;
+default-lease-time 6000;
+max-lease-time 7200;
+ddns-update-style none;
+authoritative;
+subnet 10.$X.$Y.0 netmask 255.255.255.0 {
+  range 10.$X.$Y.20 10.$X.$Y.250;
+  option routers 10.$X.$Y.10;
+  option broadcast-address 10.$X.$Y.255;
+}
+EOF
+systemctl restart isc-dhcp-server
+
+touch /var/log/esckere.log
+chmod 644 /var/log/esckere.log  # безопаснее, чем 777
+
+grep -q 'd_esckere' /etc/syslog-ng/syslog-ng.conf ||
+cat >> /etc/syslog-ng/syslog-ng.conf <<EOF
+
+destination d_esckere { file("/var/log/esckere.log"); };
+source s_net { udp(port(514)); };
+filter f_dhcp { program(dhclient); };
+log { source(s_net); filter(f_dhcp); destination(d_esckere); };
+EOF
+
+systemctl restart syslog-ng
+clear
+cat << 'EOF'
+
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣤⣄⡀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡸⠋⠀⠘⣇⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⠇⠀⠀⠀⢸⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡜⠀⠀⠀⠀⢸⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⠇⠀⠀⠀⠀⢸⠇⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡎⠀⠀⠀⠀⠀⢸⠀⠀⠀
+⠀⠀⢀⣀⣀⣀⠀⠀⠀⠀⠀⢀⣀⣤⡤⠤⠤⠤⠤⢤⣤⣀⡤⢖⡿⠛⠉⢳⠀⠀⠀⠀⠀⢸⠀⠀⠀
+⠀⢼⠁⠉⠉⠛⠻⢭⡓⠒⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⣏⠀⠀⠀⢸⠀⠀⠀⠀⠀⡤⠀⠀⠀
+⠀⠸⡄⠀⠀⠀⠀⢸⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠂⠀⠀⡜⠀⠀⠀⠀⢀⡇⠀⠀⠀
+⠀⠀⢷⠀⠀⠀⠠⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢣⢠⠏⠀⠀⠀⠀⢸⠃⠀⠀⠀
+⠀⠀⠈⢧⠀⢀⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡞⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀
+⠀⠀⠀⠈⢳⡈⠁⠀⠀⠀⠀⠀⣀⡀⠀⠀⠀⠀⠀⠀⠀⣶⣶⣦⠀⠀⢹⠀⠀⠀⠀⠀⡎⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⢠⣾⣟⣹⡄⠀⠀⠀⠀⡀⠀⣿⣿⣿⡇⠀⢈⣧⠤⠤⠶⠶⢷⠒⠒⠂⠀
+⠀⠀⢀⣀⣠⡧⠄⠀⠀⠀⣾⣿⣿⣿⠇⠀⠀⠀⠙⠁⠀⠙⠻⠿⠃⠀⠨⣼⣤⣀⡀⠀⠈⢧⠀⠀⠀
+⠘⠉⠁⠀⢸⣤⡤⠀⠀⠀⠛⢿⡿⠋⠀⠀⠀⠀⠴⠦⠀⠀⠀⠀⠀⠐⣲⣯⡀⠀⠈⠙⠓⠺⣧⣄⡀
+⠀⣀⡤⠚⠉⢳⡴⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡼⠃⠀⠈⠓⢦⡀⠀⠀⢸⠀⠈
+⠀⠁⠀⢀⡔⠉⠙⡶⢄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠴⠚⠁⠀⠀⠀⠀⠀⠀⠈⠓⠆⠀⡇⠀
+⠀⠀⠰⠋⠀⠀⢸⡇⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠁⠀
+⠀⠀⠀⠀⠀⠀⠈⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡎⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠹⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠙⢆⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠄⠀⢰⠇⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⠶⠺⣇⠀⣀⡜⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢱⡄⠀⠀⠀⠹⡟⠒⢢⡀⠀⠀⠀⠀⢀⡏⠀⠀⠀⠈⠉⠉⠁⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣄⠀⠀⢀⡇⠀⠀⠻⣄⠀⠀⠀⡸⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢷⠶⠋⠀⠀⠀⠀⠈⣣⠶⠖⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+EOF
+
+echo -e "\n✅ Все успешно выполнено!\n"
